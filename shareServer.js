4958ff975060f5a523fcf4faa1526a7a0805b7c6
rliebz <rliebz@gmail.com>
2014-11-04 10:37:26 -0500

    Create basic collaborative textarea.

---
+++ shareServer.js
@@ -0,0 +1,52 @@
+var livedb = require('livedb');
+var backend = livedb.client(livedb.memory());
+
+var sharejs = require('share');
+var share = sharejs.server.createClient({backend: backend});
+
+var Duplex = require('stream').Duplex;
+var browserChannel = require('browserchannel').server;
+
+var express = require('express');
+var server = express();
+
+// Serve static files
+server.use(express.static(sharejs.scriptsDir));
+
+server.use(browserChannel(function(client) {
+
+  var stream = new Duplex({objectMode: true});
+
+  stream._read = function() {};
+  stream._write = function(chunk, encoding, callback) {
+    if (client.state !== 'closed') {
+      client.send(chunk);
+    }
+    callback();
+  };
+
+  client.on('message', function(data) {
+    stream.push(data);
+  });
+
+  client.on('close', function(reason) {
+    stream.push(null);
+    stream.emit('close');
+  });
+
+  stream.on('end', function() {
+    client.close();
+  });
+
+  // Give the stream to sharejs
+  return share.listen(stream);
+}));
+
+server.get('/', function(req, res) {
+    res.sendfile('index.html');
+});
+
+port = 7007;
+server.listen(port, function() {
+    console.log('Server running at http://127.0.0.1:' + port);
+});
